8e0c043386613918f0b80d3c714c3974a774f0d5
aastorgas <aastorgas@gmail.com>
2020-09-15 21:40:55 +0000

    cambios

--- chapter_vertx/lab/vertx-microtrader/quote-generator/src/main/java/io/vertx/workshop/quote/GeneratorConfigVerticle.java
+++ chapter_vertx/lab/vertx-microtrader/quote-generator/src/main/java/io/vertx/workshop/quote/GeneratorConfigVerticle.java
@@ -42,12 +42,16 @@ public class GeneratorConfigVerticle extends AbstractVerticle {
 
                     // Deploy the verticle with a configuration
                     // TODO: MarketDataVerticle
-
-                    .toList()
-            )
+                    .flatMapSingle(company -> vertx.rxDeployVerticle(MarketDataVerticle.class.getName(),
+                        new DeploymentOptions().setConfig(company)))
+                                        .toList()
+                                )
 
             // Deploy another verticle
             // TODO: RestQuoteAPIVerticle
+
+            .flatMap(l -> vertx.rxDeployVerticle(RestQuoteAPIVerticle.class.getName()))
+
 
             .flatMap(x -> discovery.rxPublish(MessageSource.createRecord("market-data", ADDRESS)))
 
